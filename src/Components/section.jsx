3accb500211deb402371ccecdeb3ce4105aab4f7
peetertarvas <peetertarvas@gmail.com>
2022-11-14 22:56:03 +0200

    g

---
+++ src/Components/section.jsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+
+export const section = () => {
+    
+    return(
+        <section>
+            <h1>Peeter Tarvas</h1>
+            <p>Lorem ipsum deakk lkanfk ono pand iinok aokn asd pmvw m w</p>
+        </section>
+    )
+}
